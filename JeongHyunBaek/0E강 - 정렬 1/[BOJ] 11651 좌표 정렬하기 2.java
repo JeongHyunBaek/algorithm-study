72aa3bcd0deac29222e3e6e48b8e7c99640adfd1
JeongHyunBaek <69346978+JeongHyunBaek@users.noreply.github.com>
2024-12-02 16:54:33 +0900

    [BOJ] 좌표 정렬하기 2

--- JeongHyunBaek/0E강 - 정렬 1/[BOJ] 11651 좌표 정렬하기 2.java
+++ JeongHyunBaek/0E강 - 정렬 1/[BOJ] 11651 좌표 정렬하기 2.java
@@ -1,8 +1,7 @@
-import java.io.*;
 import java.util.*;
 
 class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
