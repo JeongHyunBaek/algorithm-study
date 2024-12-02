c3fbd251c0644dcdb34363e8313cbb913bb2fabb
JeongHyunBaek <69346978+JeongHyunBaek@users.noreply.github.com>
2024-12-02 15:45:58 +0900

    [BOJ] 수 정렬하기 2

---
+++ JeongHyunBaek/0E강 - 정렬 1/[BOJ] 2751 수 정렬하기 2.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        List<Integer> list = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < n; i++) {
+            list.add(sc.nextInt());
+        }
+
+        Collections.sort(list);
+
+        for (int num : list) {
+            sb.append(num).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
