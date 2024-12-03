09029aa72251e6e4b13c301e0b024b449c643241
JeongHyunBaek <69346978+JeongHyunBaek@users.noreply.github.com>
2024-12-03 17:55:58 +0900

    [BOJ] 1, 2, 3 더하기

---
+++ JeongHyunBaek/10강 - 다이나믹 프로그래밍/[BOJ] 9095 1, 2, 3 더하기.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        for (int i = 0; i < t; i++) {
+            int n = sc.nextInt();
+            int[] dp = new int[11];
+            dp[1] = 1;
+            dp[2] = 2;
+            dp[3] = 4;
+
+            for (int j = 4; j <= n; j++) {
+                dp[j] = dp[j - 1] + dp[j - 2] + dp[j - 3];
+            }
+            System.out.println(dp[n]);
+        }
+    }
+}
