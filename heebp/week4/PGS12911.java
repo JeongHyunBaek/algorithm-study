8a69c10bb71d1aae4b7aee232921d4edeb9eb813
heebp <skdnfl4343@gmail.com>
2024-12-19 01:24:18 +0900

    [PGS] 다음 큰 숫자

---
+++ heebp/week4/PGS12911.java
@@ -0,0 +1,24 @@
+package heebp.week4;
+
+public class PGS12911 {
+    public int solution(int n) {
+        int answer = 0;
+        int nCnt = countOne(Integer.toBinaryString(n));
+        while(true){
+            n++;
+            if(nCnt == countOne(Integer.toBinaryString(n)))
+                break;
+        }
+        answer = n;
+        return answer;
+    }
+
+    int countOne(String b){
+        int cnt = 0;
+        for(int i = 0; i < b.length(); i++){
+            if(b.charAt(i) == '1')
+                cnt++;
+        }
+        return cnt;
+    }
+}
