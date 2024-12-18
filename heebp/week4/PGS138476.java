a44c15a419e9bf99e3243b48b4ab08e9779bbc1f
heebp <skdnfl4343@gmail.com>
2024-12-19 01:26:04 +0900

    [PGS] 귤 고르기

---
+++ heebp/week4/PGS138476.java
@@ -0,0 +1,25 @@
+package heebp.week4;
+import java.util.*;
+
+public class PGS138476 {
+    public int solution(int k, int[] tangerine) {
+        int answer = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int t : tangerine){
+            map.put(t, map.getOrDefault(t, 0) + 1);
+        }
+        List<Integer> countList = new ArrayList<>();
+        for(Integer value : map.values()){
+            countList.add(value);
+        }
+        Collections.sort(countList, Collections.reverseOrder());
+        int sum = 0;
+        for(Integer count : countList){
+            if(sum >= k)
+                break;
+            sum += count;
+            answer++;
+        }
+        return answer;
+    }
+}
